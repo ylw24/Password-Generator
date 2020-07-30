483b6ce0f989aee5937cd58c61f56bbcdde20533
Linda Wang <61102300+ylw24@users.noreply.github.com>
2020-07-30 17:49:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-import string
-import itertools
+loop = True
+
 
 def password():
     import random
@@ -13,11 +12,13 @@ def password():
     password = ''.join(password)
     return password
 
+
 def GRN():
     number = []
     for i in range(5):
-        number.append(str(random.randint(1,10)))
+        number.append(str(random.randint(1, 10)))
     return number
+
 
 def GRL():
     letter = []
@@ -25,19 +26,31 @@ def GRL():
         letter.append(random.choice(string.ascii_letters))
     return letter
 
+
 def GRS():
     symbol = []
     for i in range(2):
         symbol.append(random.choice(string.punctuation))
     return symbol
 
-while True:
+
+while loop:
     print("What password type would you like to generate?")
     print("a) standard 10-digit password")
+    print("b) exit")
     action = input()
     if action == 'a':
         print(password())
+        print("\nWould you like another/different password?")
+        repeat = input("Y/N \n")
+        if repeat == 'Y':
+            print('')
+            continue
+        if repeat == 'N':
+            break
+
+    if action == 'b':
         break
     else:
-        print("invalid input, please enter a letter.")
+        print("invalid input, please enter a letter. \n")
         continue
